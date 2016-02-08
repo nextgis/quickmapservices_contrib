19c5af8652d9e351870de1904d0c9b7371f88d3a
Roland Kaiser <kardinal.eros@gmail.com>
2016-02-08 12:13:52 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#	mkdir ~/.qgis2/QuickMapServices/Contribute/data_sources/bayernatlas
+cp -R ~/Documents/quickmapservices_contrib/data_sources/bayernatlas ~/.qgis2/QuickMapServices/Contribute/data_sources/bayernatlas
+
+#	mkdir ~/.qgis2/QuickMapServices/Contribute/groups/bayernatlas
+cp -R ~//Documents/quickmapservices_contrib/groups/bayernatlas ~/.qgis2/QuickMapServices/Contribute/groups/bayernatlas
